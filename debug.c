2092eafa38de046398ec4e33f9aa0d54179e03c7
TeraSurror <harshshelar22@gmail.com>
2024-10-22 14:17:21 -0400

    basic debigger

---
+++ debug.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+#include "debug.h"
+
+void disassembleChunk(Chunk *chunk, const char *name)
+{
+    printf("== %s ==\n", name);
+
+    for (int offset = 0; offset < chunk->count;)
+    {
+        offset = disassembleInstruction(chunk, offset);
+    }
+}
+
+int disassembleInstruction(Chunk *chunk, int offset)
+{
+    print("%04d", offset);
+
+    u_int8_t instruction = chunk->code[offset];
+
+    switch (instruction)
+    {
+    case OP_RETURN:
+        return simpleInstruction("OP_RETURN", offset);
+
+    default:
+        printf("Unkown opcode %d\n", instruction);
+        return offset + 1;
+    }
+}
+
+static int simpleInstruction(const char *name, int offset)
+{
+    printf("%s\n", name);
+    return offset + 1;
+}
