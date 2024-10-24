f55161ccca9edb29092640c3117de51fc2999e52
TeraSurror <harshshelar22@gmail.com>
2024-10-23 20:49:40 -0400

    added constants

--- chunk.h
+++ chunk.h
@@ -3,11 +3,13 @@
 
 #include "common.h"
 #include "memory.h"
+#include "value.h"
 
 // Operation code corresponding to various operations
 typedef enum
 {
-    OP_RETURN, // Return from the current function
+    OP_CONSTANT, // Load constant for use
+    OP_RETURN,   // Return from the current function
 } OpCode;
 
 // Defines series of instructions
@@ -16,10 +18,13 @@ typedef struct
     int count;
     int capacity;
     uint8_t *code;
+    int *lines;
+    ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk *chunk);
 void freeChunk(Chunk *chunk);
-void writeChunk(Chunk *chunk, uint8_t byte);
+void writeChunk(Chunk *chunk, uint8_t byte, int line);
+int addConstant(Chunk *chunk, Value value);
 
 #endif
