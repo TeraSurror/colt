a7e7e6494c82b60b676acac6b9b62ea94120bd61
TeraSurror <harshshelar22@gmail.com>
2024-10-24 20:22:23 -0400

    VM start

---
+++ vm.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+#include "vm.h"
+#include "common.h"
+
+VM vm;
+
+static InterpretResult run()
+{
+#define READ_BYTE() (*vm.ip++)
+#define READ_CONSTANT() (*vm.ip++)
+
+    for (;;)
+    {
+        uint8_t instruction;
+        switch (instruction = READ_BYTE())
+        {
+        case OP_CONSTANT:
+            Value constant = READ_CONSTANT();
+            printValue(constant);
+            printf("\n");
+            break;
+        case OP_RETURN:
+            return INTERPRET_OK;
+        }
+    }
+}
+
+void initVM()
+{
+}
+
+void freeVM()
+{
+}
+
+InterpretResult interpret(Chunk *chunk)
+{
+    vm.chunk = chunk;
+    vm.ip = vm.chunk->code;
+    return run();
+}
