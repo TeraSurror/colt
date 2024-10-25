9270d1b89b6450ecca203f0becb18df0a15a1922
TeraSurror <harshshelar22@gmail.com>
2024-10-25 16:45:35 -0400

    formmating change to clang-format

--- chunk.h
+++ chunk.h
@@ -8,18 +8,18 @@
 // Operation code corresponding to various operations
 typedef enum
 {
-    OP_CONSTANT, // Load constant for use
-    OP_RETURN,   // Return from the current function
+  OP_CONSTANT, // Load constant for use
+  OP_RETURN,   // Return from the current function
 } OpCode;
 
 // Defines series of instructions
 typedef struct
 {
-    int count;
-    int capacity;
-    uint8_t *code;
-    int *lines;
-    ValueArray constants;
+  int count;
+  int capacity;
+  uint8_t *code;
+  int *lines;
+  ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk *chunk);
--- vm.c
+++ vm.c
@@ -1,42 +1,34 @@
 #include <stdio.h>
 
-#include "vm.h"
 #include "common.h"
+#include "vm.h"
 
 VM vm;
 
-static InterpretResult run()
-{
+static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (*vm.ip++)
 
-    for (;;)
-    {
-        uint8_t instruction;
-        switch (instruction = READ_BYTE())
-        {
-        case OP_CONSTANT:
-            Value constant = READ_CONSTANT();
-            printValue(constant);
-            printf("\n");
-            break;
-        case OP_RETURN:
-            return INTERPRET_OK;
-        }
+  for (;;) {
+    uint8_t instruction;
+    switch (instruction = READ_BYTE()) {
+    case OP_CONSTANT:
+      Value constant = READ_CONSTANT();
+      printValue(constant);
+      printf("\n");
+      break;
+    case OP_RETURN:
+      return INTERPRET_OK;
     }
+  }
 }
 
-void initVM()
-{
-}
+void initVM() {}
 
-void freeVM()
-{
-}
+void freeVM() {}
 
-InterpretResult interpret(Chunk *chunk)
-{
-    vm.chunk = chunk;
-    vm.ip = vm.chunk->code;
-    return run();
+InterpretResult interpret(Chunk *chunk) {
+  vm.chunk = chunk;
+  vm.ip = vm.chunk->code;
+  return run();
 }
